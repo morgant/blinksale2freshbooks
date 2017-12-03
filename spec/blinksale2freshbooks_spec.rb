64c328a06e753f24e697d2c736b3641dbe8176ba
Morgan Aldridge <morgant@makkintosshu.com>
2017-12-03 14:51:57 -0500

    Fix for version spec failure.

--- spec/blinksale2freshbooks_spec.rb
+++ spec/blinksale2freshbooks_spec.rb
@@ -1,11 +1,8 @@
 require "spec_helper"
 
-RSpec.describe Blinksale2freshbooks do
+RSpec.describe Blinksale2FreshBooks do
   it "has a version number" do
-    expect(Blinksale2freshbooks::VERSION).not_to be nil
+    expect(Blinksale2FreshBooks::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
