b0ff5a7e349d78be3e2a858e9914a13abb035ff0
Morgan Aldridge <morgant@makkintosshu.com>
2018-01-20 17:47:59 -0500

    Changed Blinksale2FreshBooks::Migration methods & args names to 'attr' instead of 'field'.

--- lib/blinksale2freshbooks/migration.rb
+++ lib/blinksale2freshbooks/migration.rb
@@ -4,41 +4,41 @@ module Blinksale2FreshBooks
 
   class Migration
 
-    attr_accessor :src, :dst, :fields
+    attr_accessor :src, :dst, :attrs
 
     def initialize(src, dst)
       raise ArgumentError if src.nil? || dst.nil?
       @src = src
       @dst = dst
-      @fields = []
+      @attrs = []
     end
 
-    def add_field_association(description, src_field, dst_field = nil)
-      raise ArgumentError if src_field.blank?
-      dst_field = src_field if dst_field.blank?
-      @fields << {
-        src: src_field,
-        dst: dst_field,
+    def add_attr_association(description, src_attr, dst_attr = nil)
+      raise ArgumentError if src_attr.blank?
+      dst_attr = src_attr if dst_attr.blank?
+      @attrs << {
+        src: src_attr,
+        dst: dst_attr,
         desc: description
       }
     end
 
     def same?
-      no_match = @fields.detect {|f| !fields_match?(f[:src], f[:dst], f[:desc])}
+      no_match = @attrs.detect {|f| !attrs_match?(f[:src], f[:dst], f[:desc])}
       (no_match.length > 0) ? true : false
     end
 
     private
 
-    def fields_match?(src_field, dst_field, description)
-      raise ArgumentError if src_field.blank? || dst_field.blank?
-      src_val = @src.send(src_field)
-      dst_val = @dst.send(dst_field)
+    def attrs_match?(src_attr, dst_attr, description)
+      raise ArgumentError if src_attr.blank? || dst_attr.blank?
+      src_val = @src.send(src_attr)
+      dst_val = @dst.send(dst_attr)
       if !(src_val.blank? && dst_val.blank?) && (src_val != src_val)
-        puts "#{description} fields don't match! #{src_field} != #{dst_field} ('#{src_val}' != '#{dst_val}')
+        puts "#{description} attrs don't match! #{src_attr} != #{dst_attr} ('#{src_val}' != '#{dst_val}')
         false
       else
-        puts "#{description} fields match. #{src_field} != #{dst_field} ('#{src_val}' == '#{dst_val}')
+        puts "#{description} attrs match. #{src_attr} != #{dst_attr} ('#{src_val}' == '#{dst_val}')
         true
       end
     end
