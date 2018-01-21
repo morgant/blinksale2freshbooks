d65aa4a9ff90c4f56735757e415bdc715119d7d2
Morgan Aldridge <morgant@makkintosshu.com>
2018-01-20 21:47:48 -0500

    Updated Migration class with #update & related methods to support migrating data in addition to comparison, plus #migration_hash method which returns a hash of key value pairs to create new objects.

--- lib/blinksale2freshbooks/migration.rb
+++ lib/blinksale2freshbooks/migration.rb
@@ -7,7 +7,7 @@ module Blinksale2FreshBooks
     attr_accessor :src, :dst, :attrs
 
     def initialize(src, dst)
-      raise ArgumentError if src.nil? || dst.nil?
+      raise ArgumentError if src.nil?
       @src = src
       @dst = dst
       @attrs = []
@@ -23,17 +23,33 @@ module Blinksale2FreshBooks
       }
     end
 
+    def migration_hash
+      hash = {}
+      @attrs.each do |attr|
+        hash[attr[:dst]] = @src.send(attr[:src])
+      end
+      hash
+    end
+
     def same?
-      no_match = @attrs.detect {|attr| !attrs_match?(attr[:src], attr[:dst], attr[:desc])}
-      no_match.nil?
+      differing = differing_attrs
+      differing.nil? || differing.empty?
+    end
+
+    def update
+      unless same?
+        differing_attrs.each do |attr|
+          migrate_attr(attr[:src], attr[:dst], attr[:desc])
+        end
+      end
     end
 
     private
 
-    def attrs_match?(src_attr, dst_attr, description)
+    def attr_match?(src_attr, dst_attr, description)
       raise ArgumentError if src_attr.blank? || dst_attr.blank?
       src_val = @src.send(src_attr)
-      dst_val = @dst.send(dst_attr)
+      dst_val = (@dst.nil?) ? nil : @dst.send(dst_attr)
       if !(src_val.blank? && dst_val.blank?) && (src_val != dst_val)
         puts "#{description} attrs don't match! #{src_attr} != #{dst_attr} ('#{src_val}' != '#{dst_val}')"
         false
@@ -41,6 +57,17 @@ module Blinksale2FreshBooks
         #puts "#{description} attrs match. #{src_attr} == #{dst_attr} ('#{src_val}' == '#{dst_val}')"
         true
       end
+    end
+
+    def differing_attrs
+      @attrs.find_all {|attr| !attr_match?(attr[:src], attr[:dst], attr[:desc])}
+    end
+
+    def migrate_attr(src_attr, dst_attr, description)
+      raise ArgumentError if @dst.nil? || src_attr.blank? || dst_attr.blank?
+      src_val = @src.send(src_attr)
+      @dst.send("#{dst_attr}=", src_val)
+      puts "#{description} attr migrated (#{src_attr} to #{dst_attr}): '#{src_val}'"
     end
 
   end
